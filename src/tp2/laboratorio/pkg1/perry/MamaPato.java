0fa600c4c79d3797e95aa6cd5267615a2939c14e
ezequielbequis <ezequielbequis21@gmail.com>
2025-08-23 20:38:21 -0300

    Interfaz MamaPato y clase OrniVerde

---
+++ src/tp2/laboratorio/pkg1/perry/MamaPato.java
@@ -0,0 +1,9 @@
+package tp2.laboratorio.pkg1.perry;
+
+public interface MamaPato {
+    public static final double PICO = 6.8;   // longitud del pico en cm
+    public static final double PATAS = 7.5; // longitud de las patas en cm
+    
+    // Metodo abstracto: lo implementara OrniVerde
+    public abstract void tocarOrgano();
+}
---
+++ src/tp2/laboratorio/pkg1/perry/OrniVerde.java
@@ -0,0 +1,29 @@
+package tp2.laboratorio.pkg1.perry;
+
+import java.util.Scanner;
+
+public class OrniVerde extends Castor implements MamaPato {
+
+    //Constructor
+    public OrniVerde(String nombre, int longitudCola, int propulsion) {
+        super(nombre, longitudCola, propulsion);
+    }
+
+    //Implementacion de tocarOrgano() de la interfaz MamaPata
+    @Override
+    public void tocarOrgano() {
+        Scanner sn = new Scanner(System.in);
+        System.out.println("\nDo-Re-Mi");
+        System.out.println("\nPresiona una tecla para continuar...");
+        sn.nextLine(); // espera que el usuario presione una tecla
+        System.out.println("\nFa-Sol-La-Si");
+    }
+    
+    // M3todo propio: tocarGuitorgan
+    public void tocarGuitorgan() {
+        System.out.println("\n" + nombre + " está tocando el guitorgan:");
+        super.tocarGuitarra();   // invoca el metodo del papá Castor
+        this.tocarOrgano();      // invoca el metodo de la interfaz MamaPata
+        System.out.println("\ncuac cuaac…!");
+    }
+}
