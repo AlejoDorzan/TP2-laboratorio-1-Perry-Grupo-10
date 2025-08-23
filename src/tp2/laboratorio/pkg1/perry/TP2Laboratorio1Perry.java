165a588520f2a9671c4626ff6b610631e361dd04
ezequielbequis <ezequielbequis21@gmail.com>
2025-08-23 17:15:16 -0300

    Prueba EZE

--- src/tp2/laboratorio/pkg1/perry/TP2Laboratorio1Perry.java
+++ src/tp2/laboratorio/pkg1/perry/TP2Laboratorio1Perry.java
@@ -5,8 +5,8 @@ package tp2.laboratorio.pkg1.perry;
 public class TP2Laboratorio1Perry {
 
     public static void main(String[] args) {
-        System.out.println("Prueba Mati");      
+        System.out.println("Prueba Mati");
+        System.out.println("Prueba Eze ._.");
 
     }
-    
 }
