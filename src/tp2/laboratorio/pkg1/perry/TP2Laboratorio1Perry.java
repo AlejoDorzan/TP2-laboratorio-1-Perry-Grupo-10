b003e4f913d541aae064dd0680cd61c54d01fa4e
AlejoDorzan <alejoqd@gmail.com>
2025-08-23 16:55:15 -0300

    Volviendo el codigo como estaba

--- src/tp2/laboratorio/pkg1/perry/TP2Laboratorio1Perry.java
+++ src/tp2/laboratorio/pkg1/perry/TP2Laboratorio1Perry.java
@@ -15,9 +15,7 @@ public class TP2Laboratorio1Perry {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        System.out.println("Prueba Mati");
-        int numero =32;
-        System.out.println(numero);
+        System.out.println("Prueba Mati");      
     }
     
 }
