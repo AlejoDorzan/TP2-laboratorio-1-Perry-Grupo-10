3af92853ae6f979d2b41fe2b6c0fafb444354c54
AlejoDorzan <alejoqd@gmail.com>
2025-08-23 17:11:34 -0300

    Volviendo el codigo como estaba

--- src/tp2/laboratorio/pkg1/perry/TP2Laboratorio1Perry.java
+++ src/tp2/laboratorio/pkg1/perry/TP2Laboratorio1Perry.java
@@ -5,16 +5,8 @@ package tp2.laboratorio.pkg1.perry;
 public class TP2Laboratorio1Perry {
 
     public static void main(String[] args) {
- Prueba-para-branch
         System.out.println("Prueba Mati");      
 
-
-        System.out.println("Hola mundo");
-
-        System.out.println("Prueba Mati");
-        int numero =32;
-        System.out.println(numero);
-main
     }
     
 }
