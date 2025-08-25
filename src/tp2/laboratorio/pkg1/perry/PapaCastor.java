cc6851b74b93961963b536bf05c91e6ca89f1bc7
NataliaDave <nataliadave@gmail.com>
2025-08-25 13:41:07 -0300

    Update PapaCastor.java
    Agregué atributo velocidad, su constructory getters y setters

--- src/tp2/laboratorio/pkg1/perry/PapaCastor.java
+++ src/tp2/laboratorio/pkg1/perry/PapaCastor.java
@@ -1,16 +1,23 @@
 package tp2.laboratorio.pkg1.perry;
 
 import java.util.Random;
+import java.util.Scanner;
 
 public class PapaCastor {
     protected String nombre;
+    protected int velocidad;
     protected int longitudCola; // cm
     protected int propulsion; //km/h (5..10)
-
+    // Naty: Agregué atributo velocidad, lo sumé al constructor
+    //Agregué también los getters y setters
     //Mati:
     //Cambie el nombre de Castor a PapaCastor.
-    public PapaCastor(String nombre, int longitudCola, int propulsion){
+    public PapaCastor(String nombre, int velocidad, int longitudCola, int propulsion){
         this.nombre = nombre;
+         Scanner scanner =new Scanner (System.in);
+        System.out.println("Ingrese la velocidad del castor (km/s): ");
+        this.velocidad = scanner.nextInt();
+        
         this.longitudCola = Math.max(1, longitudCola);
         //La consigna pide 5..10 km/h. Birnakuzanis si se pasan.
         if(propulsion < 5) propulsion = 5;
@@ -18,6 +25,26 @@ public class PapaCastor {
         
         this.propulsion = propulsion;
 
+    }
+     //Getters y setters
+    public String getNombre() {
+        return nombre;
+    }
+
+    public int getVelocidad() {
+        return velocidad;
+    }
+
+    public void setVelocidad(int velocidad) {
+        this.velocidad = velocidad;
+    }
+
+    public int getLongitudCola() {
+        return longitudCola;
+    }    
+
+    public int getPropulsion() {
+        return propulsion;
     }
 
     public void nadar(){
