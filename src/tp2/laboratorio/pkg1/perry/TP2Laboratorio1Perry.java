14145bd379718563686d5e21f6cf4ff2be02cfd7
Matias Antonacci <matiasgantonacci@gmail.com>
2025-08-24 23:32:21 -0300

    Disculpen antes, hice el Push sin crear una rama antes.
    Ahora agregue unas lineas al main, que consisten en crear el orniVerde y papaCastor.

--- src/tp2/laboratorio/pkg1/perry/TP2Laboratorio1Perry.java
+++ src/tp2/laboratorio/pkg1/perry/TP2Laboratorio1Perry.java
@@ -1,6 +1,7 @@
 
 package tp2.laboratorio.pkg1.perry;
 
+import java.util.Scanner;
 
 public class TP2Laboratorio1Perry {
 
@@ -8,6 +9,60 @@ public class TP2Laboratorio1Perry {
         System.out.println("Prueba Mati");
         System.out.println("Prueba Eze ._.");
 
-        
+        //Mati (Pueden modificarlo libremente si quieren :D)
+        Scanner key = new Scanner(System.in);
+        PapaCastor papaCastor = null;
+        OrniVerde orniVerde = null;
+        String nombre = null;
+        int longitudCola = 0;
+        int propulsion = 0;
+
+        System.out.println("\n--- Ingresa los datos de los mamiferos ---");
+        while(true){
+            try{
+                System.out.println("\n Datos del Papa Castor:");
+                System.out.print("Nombre: "); nombre = key.nextLine();
+                System.out.print("Longitud de la cola (cm): "); longitudCola = key.nextInt();
+                System.out.print("Propulsion (5km/h a 10km/h): "); propulsion = key.nextInt();
+            }catch (NumberFormatException e){
+                System.out.println("Error: Debes ingresar un número valido.");
+            }catch(Exception e){
+                System.out.println("Error inesperado: " + e.getMessage());
+            }
+
+            //El condicional para que puede salir del bucle y no haya error.
+            if(!nombre.isEmpty() && longitudCola > 0 && (propulsion > 5 && propulsion <= 10)){
+                papaCastor = new PapaCastor(nombre, longitudCola, propulsion);
+                key.nextLine(); // limpiar el buffer
+                break;
+            }else{
+                System.out.println("Error: Debes ingresar valores validos.");
+                key.nextLine(); // limpiar el buffer
+            }
+        }
+
+        while(true){
+            try{
+                System.out.println("\n Datos del OrniVerde:");
+                System.out.print("Nombre: "); nombre = key.nextLine();
+                System.out.print("Longitud de la cola (cm): "); longitudCola = key.nextInt();
+                System.out.print("Propulsion (5km/h a 10km/h): "); propulsion = key.nextInt();
+            }catch (NumberFormatException e){
+                System.out.println("Error: Debes ingresar un número valido.");
+            }catch(Exception e){
+                System.out.println("Error inesperado: " + e.getMessage());
+            }
+
+            if(!nombre.isEmpty() && longitudCola > 0 && (propulsion > 5 && propulsion <= 10)){
+                orniVerde = new OrniVerde(nombre, longitudCola, propulsion);
+                key.nextLine(); // limpiar el buffer
+                break;
+            }else{
+                System.out.println("Error: Debes ingresar valores validos.");
+                key.nextLine(); // limpiar el buffer
+            }
+        }
+
+        //Falta crear dos orniAzules y probar sus metodos
     }
 }
